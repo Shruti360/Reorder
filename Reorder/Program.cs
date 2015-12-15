277780da72f00a45d30d0d45a9739996aae28c81
Shruti360 <shrutis@360logica.com>
2015-12-15 11:34:08 +0530

    6th commit

--- Reorder/Program.cs
+++ Reorder/Program.cs
@@ -4,14 +4,15 @@ namespace Reorder
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static void Main (string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.WriteLine("First commit");
-			Console.WriteLine("Second commit");
-			Console.WriteLine("Third commit");
-			Console.WriteLine("Fourth commit");
-			Console.WriteLine("5th commit");
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("First commit");
+			Console.WriteLine ("Second commit");
+			Console.WriteLine ("Third commit");
+			Console.WriteLine ("Fourth commit");
+			Console.WriteLine ("5th commit");
+			Console.WriteLine ("6th commit");
 		}
 	}
 }
